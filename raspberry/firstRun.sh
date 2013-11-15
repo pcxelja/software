724231a5753637333e2b71547536fc02f9d56212
Pawel Grygorowicz <pcxelja@gmail.com>
2013-11-15 22:35:45 +0100

    add new install guvciew and corect file

--- raspberry/firstRun.sh
+++ raspberry/firstRun.sh
@@ -6,6 +6,7 @@ sudo apt-get install vim
 sudo apt-get install ntpdate
 sudo apt-get install git
 sudo apt-get install motion
-sudo apt-get install htop`
+sudo apt-get install htop
 sudo apt-get install iptraf
 sudo apt-get install libjpeg62 #paczka do motion-mmal
+sudo apt-get install guvcview
