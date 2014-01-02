5ac1899556a4a9ce16ef06eaf9e8989a90fe0248
Pawel Grygorowicz <pcxelja@gmail.com>
2014-01-02 17:07:49 +0100

    addes write data to file

--- getTempWithSensor/TakeTemp.py
+++ getTempWithSensor/TakeTemp.py
@@ -1,9 +1,9 @@
+import datetime
 print "----  Get Temperature with sensor ----"
 print "autor: Pawel Grygorowicz"
-print "ver 1.1"
+print "ver 1.2"
 print "--------------------------------------"
-
-def getTempWithSensor(sensor):
+def getTempWithSensor(sensor, fileTxt):
 	file  = open(sensor)
 	data = file.read()
 	file.close()
@@ -11,17 +11,32 @@ def getTempWithSensor(sensor):
 	temperaturedata = secondline.split(" ")[9]
 	temperature = float(temperaturedata[2:])
 	temperature = temperature / 1000
-	print "Temperatura: "	
+	print "Temperatura: "
 	print temperature
-	return temperature
+	#return temperature
+	fileTxt = open(fileTxt, 'a')
+	fileTxt.write(str(temperature))
+	fileTxt.write(" ")
+	fileTxt.close()
 
-def writeToFile(file, temperature):
-	file = open (file)
+def writeToFile(file, data):
+	file = open(file, 'a')
+	file.write(data)
+	file.write(" ")
+	file.close()
+
+date = str(datetime.datetime.now())
+
 
 if __name__ == "__main__":
+
+	writeToFile(file = "date.txt", data = date)
+
 	w1 = "/sys/bus/w1/devices/28-000004631ea4/w1_slave"
-	getTempWithSensor(sensor = w1)
+	getTempWithSensor(sensor = w1, fileTxt = "w1.txt")
+	#writeToFile(file = "w1.txt", data = temperature)
 
 	w2 = "/sys/bus/w1/devices/28-000004b20553/w1_slave"
-	getTempWithSensor(sensor = w2)
+	getTempWithSensor(sensor = w2, fileTxt = "w2.txt")
+	#writeToFile(file = "w2.txt", data = temperature)
 
