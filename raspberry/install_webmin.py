fc0f52feb8293f56ea0f2e2120f119abbefa5d82
Pawel Grygorowicz <pcxelja@gmail.com>
2014-02-12 21:14:06 +0100

    added script wembmin install

---
+++ raspberry/install_webmin.py
@@ -0,0 +1,5 @@
+sudo wget http://prdownloads.sourceforge.net/webadmin/webmin-1.580.tar.gz
+sudo tar -zxvf webmin-1.580.tar.gz
+sudo mkdir /var/www/webmin
+cd webmin-1.580
+sudo sh setup.sh /var/www/webmin
