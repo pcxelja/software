c291e3ca96f2d1dba50668e7ea4ec535b396e695
Pawel Grygorowicz <pcxelja@gmail.com>
2014-03-11 19:38:05 +0100

    corrected install_tplink

--- raspberry/install_tplink.sh
+++ raspberry/install_tplink.sh
@@ -1,4 +1,4 @@
-sudo su -
+#sudo su -
 #wget http://raspishop.pl/downloads/8188eu.ko-raspbian -O /lib/modules/`uname -r`/kernel/drivers/net/wireless/8188eu.ko
 sudo depmod -a
 sed -i 's/^exit 0/modprobe 8188eu\nexit 0/' /etc/rc.local
