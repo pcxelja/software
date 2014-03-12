7752ed197bfb621acc56aafc1f850d790932041b
pcxelja <pcxelja@gmail.com>
2014-03-12 08:27:48 +0100

    Added new methods, read and write with/to ports PiFace

--- monitoring_system/piFace.py
+++ monitoring_system/piFace.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
+import piface.pfio as pfio
+
+pfio.init()
 
 class PiFacePort:
-	pass
+	
+	def read_input(port):
+		#global stan
+		stan = pfio.digital_read(port)
+		return stan
+		
+	def write_output(port,stan):
+		pfio.digital_write(port,stan)
